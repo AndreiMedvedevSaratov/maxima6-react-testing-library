ae8f147989a4bfdfdbb2036627c10168b4c36640
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-07-30 15:26:00 +0400

    update in lesson 2

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,16 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
+  // 1
   // const { getByText } = render(<App />);
   // const linkElement = getByText(/learn react/i);
   // expect(linkElement).toBeInTheDocument();
 
+  // 2
+  // render(<App />);
+  // screen.debug();
+
+  // 3
   const { asFragment } = render(<App />);
   expect(asFragment(<App />)).toMatchSnapshot();
 });
