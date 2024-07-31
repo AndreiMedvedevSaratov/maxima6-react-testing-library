9e85f82cf80e5e1a2592f1cc3df226907219d11c
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-07-31 16:45:51 +0400

    update in lesson 7

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,3 @@
-import React from "react";
 import axios from "axios";
 import { render, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
@@ -13,8 +12,10 @@ const hits = [
 describe("App", () => {
   it("fetches news from an API", async () => {
     axios.get.mockImplementationOnce(() => Promise.resolve({ data: { hits } }));
+
     const { getByRole, findAllByRole } = render(<App />);
     userEvent.click(getByRole("button"));
+
     const items = await findAllByRole("listitem");
     expect(items).toHaveLength(2);
     // Additional
@@ -26,8 +27,10 @@ describe("App", () => {
 
   it("fetches news from an API and reject", async () => {
     axios.get.mockImplementationOnce(() => Promise.reject(new Error()));
+
     const { getByRole, findByText } = render(<App />);
     userEvent.click(getByRole("button"));
+
     const message = await findByText(/Something went wrong/);
     expect(message).toBeInTheDocument();
   });
@@ -35,8 +38,10 @@ describe("App", () => {
   it("fetches news from an API (alternative)", async () => {
     const promise = Promise.resolve({ data: { hits } });
     axios.get.mockImplementationOnce(() => promise);
+
     const { getByRole, getAllByRole } = render(<App />);
     userEvent.click(getByRole("button"));
+
     await act(() => promise);
     expect(getAllByRole("listitem")).toHaveLength(2);
   });
