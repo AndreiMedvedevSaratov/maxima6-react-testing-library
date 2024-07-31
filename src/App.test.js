240172903bcc62b3d43d62e1cc25a62c56555e89
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-07-31 15:52:05 +0400

    update in lesson 3

--- src/App.test.js
+++ src/App.test.js
@@ -6,10 +6,15 @@ describe("App", () => {
   it("renders App component", () => {
     render(<App />);
     expect(screen.getByText(/Search:/i)).toBeInTheDocument();
-    expect(screen.getByRole('textbox')).toBeInTheDocument();
+
+    // Смотрим разметку
+    // screen.getByRole("");
+
+    // Добавляем тесты, а потом добавляем в разметку
+    expect(screen.getByRole("textbox")).toBeInTheDocument();
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
-    expect(screen.getByPlaceholderText('search text...')).toBeInTheDocument();
-    expect(screen.getByAltText('search image')).toBeInTheDocument();
-    expect(screen.getByDisplayValue('')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText("search text...")).toBeInTheDocument();
+    expect(screen.getByAltText("search image")).toBeInTheDocument();
+    expect(screen.getByDisplayValue("")).toBeInTheDocument();
   });
 });
