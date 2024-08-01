738e4695505722b2cecdea27a105a7c54cd8f365
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-08-01 10:19:22 +0400

    update in lesson 9

--- src/ReactRouter.test.js
+++ src/ReactRouter.test.js
@@ -44,31 +44,32 @@ const RouterComponent = () => (
   </>
 );
 
-const renderWithRouter = (
-  component,
-  {
-    route = "/",
-    history = createMemoryHistory({ initialEntries: [route] }),
-  } = {}
-) => {
-  const Wrapper = ({ children }) => (
-    <Router history={history}>{children}</Router>
-  );
-  return {
-    ...render(component, { wrapper: Wrapper }),
-    history,
-  };
-};
+// const renderWithRouter = (
+//   component,
+//   {
+//     route = "/",
+//     history = createMemoryHistory({ initialEntries: [route] }),
+//   } = {}
+// ) => {
+//   const Wrapper = ({ children }) => (
+//     <Router history={history}>{children}</Router>
+//   );
+//   return {
+//     ...render(component, { wrapper: Wrapper }),
+//     history,
+//   };
+// };
 
 describe("React Router", () => {
   it("should render the home page", () => {
-    // const history = createMemoryHistory();
-    // const { container, getByTestId } = render(
-    //   <Router history={history}>
-    //     <RouterComponent />
-    //   </Router>
-    // );
-    const { container, getByTestId } = renderWithRouter(<RouterComponent />);
+    const history = createMemoryHistory();
+    const { container, getByTestId } = render(
+      <Router history={history}>
+        <RouterComponent />
+      </Router>
+    );
+
+    // const { container, getByTestId } = renderWithRouter(<RouterComponent />);
     const navbar = getByTestId("navbar");
     const link = getByTestId("home-link");
     expect(container.innerHTML).toMatch("Home page");
@@ -76,42 +77,45 @@ describe("React Router", () => {
   });
 
   it("should navigate to the contact page", () => {
-    // const history = createMemoryHistory();
-    // const { container, getByTestId } = render(
-    //   <Router history={history}>
-    //     <RouterComponent />
-    //   </Router>
-    // );
-    const { container, getByTestId } = renderWithRouter(<RouterComponent />);
+    const history = createMemoryHistory();
+    const { container, getByTestId } = render(
+      <Router history={history}>
+        <RouterComponent />
+      </Router>
+    );
+
+    // const { container, getByTestId } = renderWithRouter(<RouterComponent />);
     fireEvent.click(getByTestId("contact-link"));
     expect(container.innerHTML).toMatch("John Doe");
   });
 
   it("should navigate to error page if route is wrong", () => {
-    // const history = createMemoryHistory();
-    // history.push("/wrong-route");
-    // const { container } = render(
-    //   <Router history={history}>
-    //     <RouterComponent />
-    //   </Router>
-    // );
-    const { container } = renderWithRouter(<RouterComponent />, {
-      route: "/wrong-route",
-    });
+    const history = createMemoryHistory();
+    history.push("/wrong-route");
+    const { container } = render(
+      <Router history={history}>
+        <RouterComponent />
+      </Router>
+    );
+
+    // const { container } = renderWithRouter(<RouterComponent />, {
+    //   route: "/wrong-route",
+    // });
     expect(container.innerHTML).toMatch("404 Error");
   });
 
   it("rendering a component that uses withRouter", () => {
-    // const history = createMemoryHistory();
-    // const route = "/some-route";
-    // history.push(route);
-    // const { getByTestId } = render(
-    //   <Router history={history}>
-    //     <LocationDisplay />
-    //   </Router>
-    // );
+    const history = createMemoryHistory();
     const route = "/some-route";
-    const { getByTestId } = renderWithRouter(<LocationDisplay />, { route });
+    history.push(route);
+    const { getByTestId } = render(
+      <Router history={history}>
+        <LocationDisplay />
+      </Router>
+    );
+
+    // const route = "/some-route";
+    // const { getByTestId } = renderWithRouter(<LocationDisplay />, { route });
     expect(getByTestId("location-display")).toHaveTextContent(route);
   });
 });
