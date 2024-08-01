8cf384bc9c6e9c2b748928ad933b3e6a9d9bbd6d
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2024-08-01 21:12:12 +0400

    update in lesson 5

--- src/App.test.js
+++ src/App.test.js
@@ -7,9 +7,11 @@ describe("App", () => {
     render(<App />);
     await screen.findByText(/Logged in as/i);
     expect(screen.queryByText(/Searches for React/)).toBeNull();
+
     fireEvent.change(screen.getByRole("textbox"), {
       target: { value: "React" },
     });
+
     expect(screen.queryByText(/Searches for React/)).toBeInTheDocument();
   });
 });
@@ -17,12 +19,16 @@ describe("App", () => {
 describe("events", () => {
   it("checkbox click", () => {
     const handleChange = jest.fn();
+
     const { container } = render(
       <input type="checkbox" onChange={handleChange} />
     );
     const checkbox = container.firstChild;
+
     expect(checkbox).not.toBeChecked();
+
     fireEvent.click(checkbox);
+
     expect(handleChange).toHaveBeenCalledTimes(1);
     expect(checkbox).toBeChecked();
   });
@@ -31,9 +37,13 @@ describe("events", () => {
     const { getByTestId } = render(
       <input type="text" data-testid="simple-input" />
     );
+
     const input = getByTestId("simple-input");
+
     expect(input).not.toHaveFocus();
+
     input.focus();
+
     expect(input).toHaveFocus();
   });
 });
